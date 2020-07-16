f6d55c9fb7b830e421e8155ea46b7cfcccddb042
Corbin March <marchcorbin@gmail.com>
2020-07-16 12:14:20 -0600

    make form submittable

--- src/MakeReservation/MakeReservation.js
+++ src/MakeReservation/MakeReservation.js
@@ -16,14 +16,18 @@ class MakeReservation extends React.Component{
     this.setState({number:num++})
   }
 
+  submitHandler() {
+    
+  }
 
   render() {
     return(
-      <form>
+      <form onSubmit={this.submitHandler}>
         <h3>Make a Reservation</h3>
         <input placeholder='name' name="name" type="text" />
         <input placeholder='date' name='date' type='date' />
         <input placeholder='time' name='time' type='number' />
+        <button type='submit'></button>
       </form>
     ) 
   }
